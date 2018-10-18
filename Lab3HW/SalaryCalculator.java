ff88cbae9f70cf4113d554614593a60d0b6d8712
Keaton Burleson <keaton.burleson@me.com>
2018-10-18 00:54:01 -0500

    Lab6 Homework

--- Lab3HW/SalaryCalculator.java
+++ Lab3HW/SalaryCalculator.java
@@ -11,111 +11,111 @@ Keaton Burleson, 2018.
 */
 
 public class SalaryCalculator{
-	static Scanner scnr = new Scanner(System.in);
-	static int grade = 0;
-	static int step = 0;
-		
-	public static void main(String[] args){
-		System.out.println("Welcome to the U.S. Federal Salary Calculator.");
-		System.out.print("Please input your grade level: ");
-		grade = scnr.nextInt();
-		System.out.print("Please input your step level: ");
-		step = scnr.nextInt();
-		System.out.println("Result: " + calculateSalary(grade, step));
-		System.out.println("Have a nice day!");
-	}
+    static Scanner scnr = new Scanner(System.in);
+    static int grade = 0;
+    static int step = 0;
+        
+    public static void main(String[] args){
+        System.out.println("Welcome to the U.S. Federal Salary Calculator.");
+        System.out.print("Please input your grade level: ");
+        grade = scnr.nextInt();
+        System.out.print("Please input your step level: ");
+        step = scnr.nextInt();
+        System.out.println("Result: " + calculateSalary(grade, step));
+        System.out.println("Have a nice day!");
+    }
 
-	// Salary calculation function
-	public static String calculateSalary(int grade, int step){
-		switch(grade){
-			case 1:
-			return getGradeOneSalary(step);
+    // Salary calculation function
+    public static String calculateSalary(int grade, int step){
+        switch(grade){
+            case 1:
+            return getGradeOneSalary(step);
 
-			case 2: 
-			return getGradeTwoSalary(step);
+            case 2: 
+            return getGradeTwoSalary(step);
 
-			case 3:
-			return getGradeThreeSalary(step);
+            case 3:
+            return getGradeThreeSalary(step);
 
-			case 4:
-			return getGradeFourSalary(step);
+            case 4:
+            return getGradeFourSalary(step);
 
-			default:
-			return "You did not input a valid grade.";
-		}
-	}
+            default:
+            return "You did not input a valid grade.";
+        }
+    }
 
-	// Calculates salary for grade one
-	// This level of abstraction (getGradeX) is not practical beyond this many grades. Simply done for cleanliness.
-	public static String getGradeOneSalary(int step){
-		switch(step){
-			case 1:
-			return "$18,765";
+    // Calculates salary for grade one
+    // This level of abstraction (getGradeX) is not practical beyond this many grades. Simply done for cleanliness.
+    public static String getGradeOneSalary(int step){
+        switch(step){
+            case 1:
+            return "$18,765";
 
-			case 2: 
-			return "$19,414";
+            case 2: 
+            return "$19,414";
 
-			case 3:
-			return "$20,039";
+            case 3:
+            return "$20,039";
 
-			default:
-			return invalidStep();
-		}
-	}
+            default:
+            return invalidStep();
+        }
+    }
 
-	// Calculates salary for grade two
-	public static String getGradeTwoSalary(int step){
-		switch(step){
-			case 1:
-			return "$21,121";
+    // Calculates salary for grade two
+    public static String getGradeTwoSalary(int step){
+        switch(step){
+            case 1:
+            return "$21,121";
 
-			case 2: 
-			return "$21,624";
+            case 2: 
+            return "$21,624";
 
-			case 3:
-			return "$22,323";
+            case 3:
+            return "$22,323";
 
-			default:
-			return invalidStep();
-		}
-	}
+            default:
+            return invalidStep();
+        }
+    }
 
-	// Calculates salary for grade three
-	public static String getGradeThreeSalary(int step){
-		switch(step){
-			case 1:
-			return "$23,045";
+    // Calculates salary for grade three
+    public static String getGradeThreeSalary(int step){
+        switch(step){
+            case 1:
+            return "$23,045";
 
-			case 2: 
-			return "$23,813";
+            case 2: 
+            return "$23,813";
 
-			case 3:
-			return "$24,581";
+            case 3:
+            return "$24,581";
 
-			default:
-			return invalidStep();
-		}
-	}
+            default:
+            return invalidStep();
+        }
+    }
 
-	// Calculates salary for grade four
-	public static String getGradeFourSalary(int step){
-		switch(step){
-			case 1:
-			return "$25,871";
+    // Calculates salary for grade four
+    public static String getGradeFourSalary(int step){
+        switch(step){
+            case 1:
+            return "$25,871";
 
-			case 2: 
-			return "$26,733";
+            case 2: 
+            return "$26,733";
 
-			case 3:
-			return "$27,595";
+            case 3:
+            return "$27,595";
 
-			default:
-			return invalidStep();
-		}
-	}
+            default:
+            return invalidStep();
+        }
+    }
 
-	public static String invalidStep(){
-		return "You did not input a valid step.";
-	}
+    public static String invalidStep(){
+        return "You did not input a valid step.";
+    }
 
 }
--- Lab5HW/RetirementCalculator.java
+++ Lab5HW/RetirementCalculator.java
@@ -1,105 +1,73 @@
 import java.util.Scanner;
 
-// Keaton Burleson 
-// Full suite retirement calculator that will tell you when you can stop putting money away AND 
-// still be able to reach your goal
 public class RetirementCalculator{
-	final static Scanner scnr = new Scanner(System.in);
-	final static int FINAL_AGE = 65;
+    final static Scanner scnr = new Scanner(System.in);
+    final static int FINAL_AGE = 65;
 
-	static int currentAge;
-	static int coastAge = -1;
-	static double initialBalance;
-	static double yearlyContribution;
-	static double interestRatePercent;
-	static double currentBalance;
-	static double targetAmount;
-	static double initialAmount;
+    static int currentAge;
+    static double initialBalance;
+    static double yearlyContribution;
+    static double interestRatePercent;
+    static double currentBalance;
+    static double targetAmount;
+    static double initialAmount;
 
-	public static void main(String[] args)
-	{
-		System.out.println("Initial amount of money:");
-		initialAmount = scnr.nextDouble();
+    public static void main(String[] args)
+    {
+        System.out.println("Initial amount of money:");
+        initialAmount = scnr.nextDouble();
 
-		System.out.println("Target amount at age " + FINAL_AGE + ":");
-		targetAmount = scnr.nextDouble();
+        System.out.println("Target amount at age " + FINAL_AGE + ":");
+        targetAmount = scnr.nextDouble();
 
-		System.out.println("Contributions per year:");
-		yearlyContribution = scnr.nextDouble();
+        System.out.println("Contributions per year:");
+        yearlyContribution = scnr.nextDouble();
 
-		System.out.println("Annual growth in percent:");
-		interestRatePercent = (scnr.nextDouble() / 100);
+        System.out.println("Annual growth in percent:");
+        interestRatePercent = (scnr.nextDouble() / 100);
+        System.out.println("(" + interestRatePercent + ")");
 
-		System.out.println("Your current age:");
-		currentAge = scnr.nextInt();
+        System.out.println("Your current age:");
+        currentAge = scnr.nextInt();
 
-		checkForRetirement();
-	}
+        calculateRetirement();
+    }
 
-	public static void checkForRetirement()
-	{
-		int retirementAge = getFinancialTargetReachedAge(false);
-		if(retirementAge > FINAL_AGE){
-			System.out.println("Sorry, that target is unreachable :(");
-		}else{
-			System.out.println("\n Congratulations! You can reach that target by saving");
-			System.out.println("your yearly contribution amount for " + coastAge + " year(s) until");
-			System.out.println("age " + retirementAge + ", then coasting the rest of the way to " + FINAL_AGE + ".");
-			System.out.println("Projected growth:");
-			System.out.println("-----------------");
-			System.out.printf("%5s%15s%15s%15s\n", "Age", "Interest", "Contribution", "End Balance");
+    public static void calculateRetirement()
+    {
+        int retirementAge = getRetirementAge(false);
+        if(retirementAge > FINAL_AGE){
+            System.out.println("Sorry, that target is unreachable :(");
+        }else{
+            System.out.println("\n Congratulations! You can reach that target by saving");
+            System.out.println("your yearly contribution amount for " + (retirementAge - currentAge) + " year(s) until");
+            System.out.println("age " + retirementAge + ", then coasting the rest of the way to " + FINAL_AGE + ".");
+            System.out.println("Projected growth:");
+            System.out.println("-----------------");
+            System.out.printf("%5s%15s%15s%15s\n", "Age", "Interest", "Contribution", "End Balance");
 
-			getFinancialTargetReachedAge(true);
-		}
-	}
+            getRetirementAge(true);
+        }
+    }
 
-	// Returns the age in which you reach your financial target
-	public static int getFinancialTargetReachedAge(Boolean printToConsole){
-		int incrementalAge = currentAge;
-		currentBalance = initialAmount;
+    public static int getRetirementAge(Boolean printToConsole){
+        int y = currentAge;
+        currentBalance = initialAmount;
 
-		while(currentBalance < targetAmount){
-			double interest = interestRatePercent * currentBalance;
-			double contribution = yearlyContribution;
+        while(currentBalance <= targetAmount && canRideOnInterest() == false){
+            double interest = interestRatePercent * currentBalance;
+            currentBalance += (interest + yearlyContribution);
+            y++;
+            if(printToConsole == true){
+                System.out.printf("%5d%15.2f%15.2f%15.2f\n", y, interest, yearlyContribution, currentBalance);
+            }
+        }
 
-			if(canStopContributions(incrementalAge) && printToConsole == true){
-				contribution = 0.0;
-				// Checks if the 'coast age' (where you can stop contributing) is not defined
-				if(coastAge == -1){
-					coastAge = incrementalAge;
-				}
-		
-			}
+        return y;
+    }
 
-			// Appends the interest and contribution to the current balance
-			currentBalance += (interest + contribution);
-			incrementalAge++;
+    public static Boolean canRideOnInterest(){
 
-			// If we're in the verbose, fun happy print time mode
-			if(printToConsole == true){
-				System.out.printf("%5d%15.2f%15.2f%15.2f\n", incrementalAge, interest, contribution, currentBalance);
-			}
-		}
-
-		return incrementalAge;
-	}
-
-	// Boolean statement that determines if you can just make your target on interest
-	public static Boolean canStopContributions(int currentAge){
-		double checkBalance = currentBalance;
-		while(currentAge <= FINAL_AGE && !(checkBalance >= targetAmount)){
-			double interest = interestRatePercent * checkBalance;
-			checkBalance += interest;
-			currentAge++;
-		}
-
-		// If the while loop breaks out, this checks if it is because of
-		// the age constraint or because of the financial constraint
-		if(checkBalance >= targetAmount && currentAge <= FINAL_AGE){
-			return true;
-		}
-
-		return false;
-	}
+    }
 
 }
