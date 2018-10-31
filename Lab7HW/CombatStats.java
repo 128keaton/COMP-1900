ab051bef4ee8d10f6310c3c210f08eb88f04a99d
Keaton Burleson <keaton.burleson@me.com>
2018-10-31 13:22:43 -0500

    Finishes CombatStats

--- Lab7HW/CombatStats.java
+++ Lab7HW/CombatStats.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
+// Gets combat statistics from the user and performs calculations on them
 public class CombatStats{
 	static final Scanner scnr = new Scanner(System.in);
 
+	// Starts the stat retrieval loop.
 	public static void main(String[] args){
 		System.out.print("How many attack stats do you want to enter? ");
 		getStatisticInput(scnr.nextInt());
@@ -11,21 +13,24 @@ public class CombatStats{
 	public static void getStatisticInput(int quantity){
 		double[] attackDamages = new double[quantity];
 
+		// For the amount specified, get the stats from the user
 		for(int i = 0; i < quantity; i++){
 			System.out.print("Attack damage " + (i + 1) + ": ");
+			// Append to the array
 			attackDamages[i] = scnr.nextDouble();
 		}
 
+		// Print the statistics
 		printStats(attackDamages);
 	}
 
+	// Calculates the max damage stat, min damage stat, and average damage stat
 	public static void printStats(double[] stats){
-		System.out.println("Stats length: " + stats.length);
-
 		double lowestDamageStat = stats[0];
 		double highestDamageStat = stats[(stats.length - 1)];
 		double totalDamageStat = 0;
 
+		// This loop takes care of the smallest damage stat and the total damage stat
 		for(int i = 0; i < stats.length; i++){
 			if(stats[i] < lowestDamageStat){
 				lowestDamageStat = stats[i];
@@ -33,15 +38,18 @@ public class CombatStats{
 			totalDamageStat += stats[i];
 		}
 
+		// This loop takes care of the largest damage stat
 		for(int i = (stats.length - 1); i >= 0; i--){
 			if(stats[i] > highestDamageStat){
 				highestDamageStat = stats[i];
 			}
 		}
 
+		// Print out the stats
 		System.out.println("Lowest damage stat: " + lowestDamageStat);
 		System.out.println("Highest damage stat: " + highestDamageStat);
 		System.out.println("Average damage stat: " + (totalDamageStat / (stats.length)));
+		System.out.println("Total damage: " + totalDamageStat);
 
 	}
 }
