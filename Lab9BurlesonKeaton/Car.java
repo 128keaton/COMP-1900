a17cb692a35deb7d6bc0ca4bfde3e5110d546711
Keaton Burleson <keaton.burleson@me.com>
2018-11-28 20:41:45 -0600

    Adds car

---
+++ Lab9BurlesonKeaton/Car.java
@@ -0,0 +1,40 @@
+
+public class Car {
+	public static String make;
+	public static String model;
+	public static String color;
+	public static double price;
+	public static double mileage;
+
+	Car(){
+		this.make = "Toyota";
+		this.model = "4Runner";
+		this.color = "White";
+		this.price = 8200.0;
+		this.mileage = 0.0;
+	}
+
+	Car(String make, String model, String color, double price){
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.price = price;
+		this.mileage = 0.0;
+	}
+
+	public void setPrice(double p){
+		this.price = p;
+	}
+
+	public void paint(String c){
+		this.color = c;
+	}
+
+	public void displayCarInfo(){
+		System.out.println("Make: " + this.make);
+		System.out.println("Model: " + this.model);
+		System.out.println("Color: " + this.color);
+		System.out.println("Price: $" + this.price);
+		System.out.println("Mileage: " + this.mileage);
+	}
+}
